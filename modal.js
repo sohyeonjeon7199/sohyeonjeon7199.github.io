bb892a8ccacaf4580556679b5735434e4c25f74c
sohyeonjeon7199 <you03332@gmail.com>
2025-11-10 14:56:21 +0900

    Add files via upload

--- modal.js
+++ modal.js
@@ -1,46 +1,46 @@
 // 프로젝트 정보 데이터
 const projects = {
   project1: {
-    title: 'Edit Design Project 1',
+    title: 'Project_UI/UX Design',
     image: 'img/portfolio/p1.jpg',
     description: '이 프로젝트는 브랜드 아이덴티티를 위한 패키지 디자인입니다. 심플하면서도 감각적인 디자인으로 제품의 가치를 극대화했습니다.'
   },
   project2: {
-    title: 'Edit Design Project 2',
-    image: 'img/portfolio/let.jpg',
-    description: '레터링 디자인 프로젝트로, 타이포그래피를 중심으로 한 비주얼 아이덴티티 작업입니다.'
+    title: 'Project_Detail page Design',
+    image: 'img/portfolio/p2.png',
+    description: '차가 주는 여유와 따뜻함을 시각적으로 표현해, 오설록의 신뢰감과 선물용 감성을 스토리로 풀어낸 비주얼 디자인입니다.'
   },
   project3: {
-    title: 'Project_SNS카드뉴스',
-    image: 'img/portfolio/pro3.jpg',
-    description: '네이밍과 로고 디자인을 통합한 브랜딩 프로젝트입니다.'
+    title: 'Project_SNS Card Design',
+    image: 'img/portfolio/p3.png',
+    description: '청취자들이 프로그램 구성을 쉽게 이해하도록 핵심 코너를 요약해 제작한 라디오 프로그램 홍보 카드뉴스입니다.'
   },
   project4: {
-    title: 'Project_리플렛',
-    image: 'img/portfolio/pro4.jpg',
-    description: '기업 로고 디자인 프로젝트로, 브랜드의 정체성을 명확히 표현했습니다.'
+    title: 'Project_3-Fold leaflet Design',
+    image: 'img/portfolio/p4.png',
+    description: '장학금 제도를 한눈에 이해할 수 있도록 일러스트와 텍스트를 조화롭게 구성한 3단 접지 리플렛 디자인입니다.'
   },
   project5: {
-    title: 'Edit Design Project 5',
+    title: 'Project_Package Design',
     image: 'img/portfolio/logo1.png',
-    description: '모던하고 세련된 로고 디자인으로 브랜드 가치를 높였습니다.'
+    description: '클라이언트의 요구를 반영해 만든 패키지 디자인입니다.'
   },
   project6: {
-    title: 'Edit Design Project 6',
-    image: 'img/portfolio/logo1.png',
-    description: '심볼과 로고타입을 결합한 통합 브랜딩 디자인입니다.'
+    title: 'Project_Poster Design',
+    image: 'img/portfolio/p6.png',
+    description: '나이키 러닝화를 기반으로 스포츠 브랜드의 철학을 감성적으로 재해석한 콘셉트 포스터 디자인입니다.'
   },
 
   project7: {
-    title: 'Edit Design Project 7',
-    image: 'img/portfolio/logo1.png',
-    description: '심볼과 로고타입을 결합한 통합 브랜딩 디자인입니다.'
+    title: 'Project_House organ Design',
+    image: 'img/portfolio/p7.png',
+    description: '조직의 소통 강화와 신뢰 구축을 위해 정보 전달력을 높이고 시각적 완성도를 강조한 인천국제공항공사 사보 리디자인 작업입니다.'
   },
 
   project8: {
-    title: 'Edit Design Project 8',
-    image: 'img/portfolio/logo1.png',
-    description: '심볼과 로고타입을 결합한 통합 브랜딩 디자인입니다.'
+    title: 'Project_Logo Design',
+    image: 'img/portfolio/p8.png',
+    description: '카페의 외형과 커피 이미지를 결합해 브랜드 정체성을 직관적으로 전달하는 로고 디자인입니다.'
   },
 };
 
